3045c886f36e24b58d1613a9425cb627fc992c5b
Marlon Braga <rdg.brg@gmail.com>
2019-06-10 01:59:29 -0400

    07-TheDom/03-events

--- thewebdev/07-TheDOM/03-events/main.js
+++ thewebdev/07-TheDOM/03-events/main.js
@@ -1,17 +1,17 @@
 let lis = document.querySelectorAll("li");
 
 for(let i = 0; i < lis.length; i++) {
-  lis[i].addEventListener("mouseover", function(){
-    this.style.color = "red";
+  lis[i].addEventListener("mouseover", function() {
+    this.classList.add("selected");
   });
 
-  lis[i].addEventListener("mouseout", function(){
-    this.style.color = "black";
+  lis[i].addEventListener("mouseout", function() {
+    this.classList.remove("selected");
   });
 }
 
 for(let i = 0; i < lis.length; i++) {
-  lis[i].addEventListener("click", function(){
-    this.classList.toggle("selected")
-  })
+  lis[i].addEventListener("click", function() {
+    this.classList.toggle("taskdone");
+  });
 }
